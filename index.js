160b56c36a4a9c5dd9844e7ec93f46617607a301
Pîrvănescu Dimitrie-Alexandru <pirvanescudimitrie@gmail.com>
2026-03-30 21:07:01 +0300

    Etapa 3

--- index.js
+++ index.js
@@ -13,8 +13,32 @@ console.log("Cale fisier", __filename);
 app.use("/resurse", express.static(path.join(__dirname, "resurse")));
 
 app.get("/", function(req, res) {
-    res.render("pagini/index");;
-})
+    res.render("pagini/index");
+});
+
+app.get("/despre", function(req, res) {
+    res.render("pagini/despre");
+});
+
+app.get("/produse", function(req, res) {
+    res.send("<h1>Produse</h1><p>Pagina de produse va fi adaugata in curand.</p>");
+});
+
+app.get("/cos", function(req, res) {
+    res.send("<h1>Cos</h1><p>Pagina de cos va fi adaugata in curand.</p>");
+});
+
+app.get("/galerie-statica", function(req, res) {
+    res.send("<h1>Galerie statica</h1><p>Pagina va fi adaugata in curand.</p>");
+});
+
+app.get("/galerie-dinamica", function(req, res) {
+    res.send("<h1>Galerie dinamica</h1><p>Pagina va fi adaugata in curand.</p>");
+});
+
+app.get("/contact", function(req, res) {
+    res.send("<h1>Contact</h1><p>Pagina de contact va fi adaugata in curand.</p>");
+});
 
 app.get("/cale/:a/:b", function(req, res) {
     res.send(parseInt(req.params.a) + parseInt(req.params.b) + "");
@@ -25,13 +49,6 @@ app.get("/cale/:a/:b", function(req, res) {
 app.get("/cale2", function(req, res) {
     res.sendFile(path.join(__dirname, "cale2.html"));
     console.log("Am primit o cerere GET pe /cale2");
-})
-
-
-app.get("/:a/:b", function(req, res) {
-    res.sendFile(path.join(__dirname, "index.html")
-
-);
 });
 
 app.listen(8080);
